3d3f1a419c8e50149ce5112b4d733a351ba3dd7f
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2024-04-18 22:17:02 +0700

    Rebuild Spotify UI
    - initial commit

---
+++ RebuildSpotifyUI/RebuildSpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebuildSpotifyUIApp.swift
+//  RebuildSpotifyUI
+//
+//  Created by Chinh on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RebuildSpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

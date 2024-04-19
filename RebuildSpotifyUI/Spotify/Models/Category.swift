3de11805701bc0b3e2dadd9293901a1ece61bd59
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2024-04-19 23:14:00 +0700

    Spotify home view
    - show genres
    - ref: https://www.youtube.com/watch?v=KDZEvX04wsc&list=PLwvDm4VfkdpiT7mKzjxfCYn_zaMC3Fmkz&index=4

---
+++ RebuildSpotifyUI/Spotify/Models/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  RebuildSpotifyUI
+//
+//  Created by Chinh on 4/19/24.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case all
+    case music
+    case podcasts
+    case audiobooks
+    case vietnamese
+    case chinese
+    case rock
+}
